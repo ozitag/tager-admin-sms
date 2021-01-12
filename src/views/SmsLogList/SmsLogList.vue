7215104a449a6738446d68e305e401f03ff722c3
seryozhabaleyko <seryozha.baleyko@gmail.com>
2021-01-12 19:22:01 +0300

    Pagination for DataTable component

--- src/views/SmsLogList/SmsLogList.vue
+++ src/views/SmsLogList/SmsLogList.vue
@@ -7,6 +7,13 @@
         :loading="isRowDataLoading"
         :error-message="errorMessage"
         :search-query="searchQuery"
+        :pagination="{
+          pageSize,
+          pageNumber,
+          pageCount,
+          disabled: isRowDataLoading,
+          pageSizes: [250, 500, 1000],
+        }"
         @change="handleChange"
       >
         <template v-slot:cell(body)="{ row }">
@@ -67,11 +74,20 @@ export default defineComponent({
       errorMessage,
       searchQuery,
       handleChange,
+      pageSize,
+      pageNumber,
+      pageCount,
     } = useDataTable<SmsLog>({
-      fetchEntityList: (params) => getSmsLogList({ query: params.searchQuery }),
+      fetchEntityList: (params) =>
+        getSmsLogList({
+          query: params.searchQuery,
+          pageNumber: params.pageNumber,
+          pageSize: params.pageSize,
+        }),
       initialValue: [],
       context,
       resourceName: 'SMS log list',
+      pageSize: 250,
     });
 
     return {
@@ -81,6 +97,9 @@ export default defineComponent({
       errorMessage,
       searchQuery,
       handleChange,
+      pageSize,
+      pageNumber,
+      pageCount,
     };
   },
 });
